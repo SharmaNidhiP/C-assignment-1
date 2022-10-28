5518b47d8acfe85c147d9c0a45b947625b1997b3
SharmaNidhiP <115006056+SharmaNidhiP@users.noreply.github.com>
2022-10-28 22:00:53 +0530

    Add files via upload

---
+++ assi1ques5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Circle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double r, AREA,Perimeter;
+            const double PI = 3.14;
+           
+            Console.WriteLine("Program to calculate the area and circumference of a circle");
+            
+            Console.Write("\nEnter the radius of circle ");
+            r = Convert.ToDouble(Console.ReadLine());
+            AREA = PI * r * r;
+            Console.WriteLine("\nThe area of circle is {0} when radius is {1}", AREA, r);
+            Console.WriteLine("\nThe circumference of circle is {0}", 2 * PI * r);
+            Perimeter = 2*PI * r;
+            Console.WriteLine("\nThe Perimeter of circle is "+Perimeter);
+          
+        }
+    }
+}
+
